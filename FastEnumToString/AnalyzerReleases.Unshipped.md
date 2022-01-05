eee8d48a1b3664f8da8e597fb6f1dc2e7de88f3d
Szöllősi Dániel <szollosi.daniel0@gmail.com>
2022-01-05 17:30:30 +0100

    Genreator updated

---
+++ FastEnumToString/AnalyzerReleases.Unshipped.md
@@ -0,0 +1,9 @@
+﻿; Unshipped analyzer release
+; https://github.com/dotnet/roslyn-analyzers/blob/main/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
+
+### New Rules
+
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+ETSG001 | Compilation | Error | EnumToStringGenerator
+ETSG002 | Compilation | Info | EnumToStringGenerator
---
+++ FastEnumToString/EnumToStringSyntaxReciever.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FastEnumToString
+{
+    internal sealed class EnumToStringSyntaxReciever : ISyntaxReceiver
+    {
+        public ICollection<EnumDeclarationSyntax> FoundEnums { get; }
+
+        public EnumToStringSyntaxReciever()
+        {
+            FoundEnums = new List<EnumDeclarationSyntax>();
+        }
+
+        /// <inheritdoc/>
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is EnumDeclarationSyntax enumDeclarationSyntax)
+            {
+                FoundEnums.Add(enumDeclarationSyntax);
+            }
+        }
+    }
+}
